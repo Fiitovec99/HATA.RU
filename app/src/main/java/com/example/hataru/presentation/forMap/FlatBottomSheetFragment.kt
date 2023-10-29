47ded524d59d8827072702c337cc8336d5c65fd5
Саня <ad.cherevko@gmail.com>
2023-10-29 20:17:21 +0300

    made simple design in FlatBottomSheetFragment, but it needs later develop

--- app/src/main/java/com/example/hataru/presentation/forMap/FlatBottomSheetFragment.kt
+++ app/src/main/java/com/example/hataru/presentation/forMap/FlatBottomSheetFragment.kt
@@ -1,20 +1,26 @@
 package com.example.hataru.presentation.forMap
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.example.hataru.R
 import com.example.hataru.databinding.FragmentFlatBottomSheetBinding
 import com.example.hataru.databinding.FragmentMapBinding
 import com.example.hataru.presentation.showToast
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-
+private const val COLLAPSED_HEIGHT = 228
 class FlatBottomSheetFragment : BottomSheetDialogFragment() {
 
     private lateinit var binding : FragmentFlatBottomSheetBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,11 +29,100 @@ class FlatBottomSheetFragment : BottomSheetDialogFragment() {
         binding = FragmentFlatBottomSheetBinding.inflate(inflater, container, false)
 
 
-        // Handle actions inside the Bottom Sheet if needed
         
 
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.apply{
+            textViewAdressFlat.setText("Мильчакова 8А")
+            textViewNumberRooms.setText("2-х комнатная")
+            textViewFlatCost.setText("3000 rub")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+
+        val density = requireContext().resources.displayMetrics.density
+
+
+        dialog?.let {
+            // Находим сам bottomSheet и достаём из него Behaviour
+            val bottomSheet =
+                it.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
+            val behavior = BottomSheetBehavior.from(bottomSheet)
+
+            // Выставляем высоту для состояния collapsed и выставляем состояние collapsed
+            behavior.peekHeight = (COLLAPSED_HEIGHT * density).toInt()
+            behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            val coordinator = (it as BottomSheetDialog).findViewById<CoordinatorLayout>(com.google.android.material.R.id.coordinator)
+            val containerLayout = it.findViewById<FrameLayout>(com.google.android.material.R.id.container)
+
+            // Надуваем наш лэйаут с кнопкой
+            val buttons = it.layoutInflater.inflate(R.layout.button, null)
+
+            // Выставляем параметры для нашей кнопки
+            buttons.layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                height = (60 * density).toInt()
+                gravity = Gravity.BOTTOM
+            }
+            // Добавляем кнопку в контейнер
+            containerLayout?.addView(buttons)
+
+            // Перерисовываем лэйаут
+            buttons.post {
+                (coordinator?.layoutParams as ViewGroup.MarginLayoutParams).apply {
+                    buttons.measure(
+                        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+                    )
+                    // Устраняем разрыв между кнопкой и скролящейся частью
+                    this.bottomMargin = (buttons.measuredHeight - 8 * density).toInt()
+                    containerLayout?.requestLayout()
+                }
+            }
+            behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    // Нам не нужны действия по этому колбеку
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                    with(binding) {
+                        // Нас интересует только положительный оффсет, тк при отрицательном нас устроит стандартное поведение - скрытие фрагмента
+                        if (slideOffset > 0) {
+                            // Делаем "свёрнутый" layout более прозрачным
+
+                            layoutCollapsed.alpha = 1 - 2 * slideOffset
+                            // И в то же время делаем "расширенный layout" менее прозрачным
+                            layoutExpanded.alpha = slideOffset * slideOffset
+
+                            // Когда оффсет превышает половину, мы скрываем collapsed layout и делаем видимым expanded
+                            if (slideOffset > 0.5) {
+                                layoutCollapsed.visibility = View.GONE
+                                layoutExpanded.visibility = View.VISIBLE
+                            }
+
+                            // Если же оффсет меньше половины, а expanded layout всё ещё виден, то нужно скрывать его и показывать collapsed
+                            if (slideOffset < 0.5 && binding.layoutExpanded.visibility == View.VISIBLE) {
+                                layoutCollapsed.visibility = View.VISIBLE
+                                layoutExpanded.visibility = View.INVISIBLE
+                            }
+                        }
+                    }
+                }
+            })
+        }
+
+    }
 
 }
