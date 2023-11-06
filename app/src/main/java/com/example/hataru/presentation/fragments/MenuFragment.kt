d2b3e315e7bdc1f0e3d5ad4d9de43616c5ca41e2
yuliya-grosheva <146519798+yuliya-grosheva@users.noreply.github.com>
2023-11-06 18:10:09 +0300

    added textView with opening WhatsUp with manager contact

--- app/src/main/java/com/example/hataru/presentation/fragments/MenuFragment.kt
+++ app/src/main/java/com/example/hataru/presentation/fragments/MenuFragment.kt
@@ -1,11 +1,16 @@
 package com.example.hataru.presentation.fragments
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.hataru.R
 import com.example.hataru.databinding.FragmentMenuBinding
 
@@ -31,10 +36,22 @@ class MenuFragment : Fragment() {
 
         binding.textView.setOnClickListener {
             //Дальнейшая реализация логики слушателя
+
+            }
+        binding.textViewOpenWhatsUp.setOnClickListener {
+            val phoneNumber = "+89959890049"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://api.whatsapp.com/send?phone=$phoneNumber")
+
+            try {
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText( activity as AppCompatActivity,"WhatsApp не установлен.", Toast.LENGTH_SHORT).show()
+            }
+        }
         }
 
 
 
     }
 
-}
