71052ddc6b69ef170ab20188e54745acfb750dd7
liliyakoshelyaeva <146521731+liliyakoshelyaeva@users.noreply.github.com>
2023-11-24 00:50:54 +0300

    Added layout fragment favorite flat

--- app/src/main/java/com/example/hataru/presentation/fragments/FavoriteFlatFragment.kt
+++ app/src/main/java/com/example/hataru/presentation/fragments/FavoriteFlatFragment.kt
@@ -1,26 +1,61 @@
 package com.example.hataru.presentation.fragments
 
+import android.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.hataru.R
+import com.example.hataru.databinding.FragmentFavoriteFlatBinding
+import com.example.hataru.databinding.FragmentMenuBinding
 
 class FavoriteFlatFragment : Fragment() {
 
-//    companion object {
+    //    companion object {
 //        fun newInstance() = FavoriteFlatFragment()
 //    }
 //
 //    private lateinit var viewModel: FavoriteFlatViewModel
+    private lateinit var binding: FragmentFavoriteFlatBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_favorite_flat, container, false)
+        binding = FragmentFavoriteFlatBinding.inflate(inflater, container, false)
+        return binding.root
     }
+    fun ShowToast(){
+        Toast.makeText(activity as AppCompatActivity, "в разработке",Toast.LENGTH_SHORT)
+                .show()
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+
+        binding.apply {
+            imageViewApartment.setOnClickListener {
+                ShowToast()
+            }
+            imageViewApartment1.setOnClickListener {
+                ShowToast()
+            }
+            imageViewApartment2.setOnClickListener {
+                ShowToast()
+            }
+            imageViewApartment3.setOnClickListener {
+                ShowToast()
+            }
+            imageViewApartment4.setOnClickListener {
+                ShowToast()
+            }
+            imageViewApartment5.setOnClickListener {
+                ShowToast()
+            }
+        }
+    }
 }
