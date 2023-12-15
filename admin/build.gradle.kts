e2c9e95457c3bb406ba4cab93826fff08760fc40
Саня <ad.cherevko@gmail.com>
2023-12-15 12:47:32 +0300

    chanded targetSDK to 34 version

--- admin/build.gradle.kts
+++ admin/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.admin"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.admin"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
