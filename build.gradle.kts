eec50f6279cbba2c2ff3041d90016631a1b09ff7
Никита Михайличенко <113203618+Fiitovec99@users.noreply.github.com>
2024-02-09 15:13:20 +0300

    update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
