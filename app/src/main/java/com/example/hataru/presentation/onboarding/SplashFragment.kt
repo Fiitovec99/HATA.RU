9a3bd2a3d00027994b981b0f4b147d3b7b68459f
CutePufik <ad.cherevko@gmail.com>
2024-03-21 14:40:10 +0300

    made navigation with registr user

--- app/src/main/java/com/example/hataru/presentation/onboarding/SplashFragment.kt
+++ app/src/main/java/com/example/hataru/presentation/onboarding/SplashFragment.kt
@@ -1,6 +1,7 @@
 package com.example.hataru.presentation.onboarding
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -10,26 +11,45 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.hataru.R
+import com.example.hataru.presentation.activities.MainActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashFragment : Fragment() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-
+        firebaseAuth = FirebaseAuth.getInstance()
         val mainHandler = Handler(Looper.getMainLooper())
         val runnable = Runnable {
             if (oBoardingFinished()){
-                findNavController().navigate(R.id.action_splashFragment_to_logInFragment)
+                if(firebaseAuth.currentUser!=null){
+                    moveToMainActivity()
+                }else{
+                    findNavController().navigate(R.id.action_splashFragment_to_logInFragment)
+                }
             } else {
-                findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
+                if(firebaseAuth.currentUser!=null){
+                    moveToMainActivity()
+                }else{
+                    findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
+                }
+
             }
         }
 
         mainHandler.postDelayed(runnable, 0) //TODO поставить 1500
 
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    private fun moveToMainActivity(){
+        findNavController().navigate(R.id.action_splashFragment_to_mainActivity)
+        requireActivity().finish()
     }
 
     private fun oBoardingFinished(): Boolean{
