2caffb222f618be94c97fc0a5c420bcb0e3e3606
CutePufik <ad.cherevko@gmail.com>
2024-03-28 21:31:39 +0300

    got rid of not existing flats

--- app/src/main/java/com/example/hataru/domain/GetFlatsUseCase.kt
+++ app/src/main/java/com/example/hataru/domain/GetFlatsUseCase.kt
@@ -5,9 +5,10 @@ import com.example.hataru.domain.entity.Roomtype
 class GetFlatsUseCase(private var rep : FlatsRep) {
 
     suspend fun getFlats() : List<Roomtype>{
+        val listWithBadId = listOf(495575,459245,518222)
         val flats = rep.getFlats()
         val flatsWithoutZeroCoordinates = flats.toList().filter{ x : Roomtype -> x.geo_data?.x?.toDouble() != 0.0 && x.geo_data?.y?.toDouble() != 0.0 }
         val pattern = Regex("^Ф[0-9]")
-        return flatsWithoutZeroCoordinates.filter { x : Roomtype -> !pattern.containsMatchIn(x.name.toString()) }
+        return flatsWithoutZeroCoordinates.filter { x : Roomtype -> !pattern.containsMatchIn(x.name.toString()) && !listWithBadId.contains(x.id!!.toInt())}
     }
 }
